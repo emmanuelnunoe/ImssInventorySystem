c40693c5d14a99ed35111449a59b722df820bc99
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2021-12-21 22:15:04 -0800

    Creation and uptdate of 3 models

--- ImssInventory.API/Models/Computer.cs
+++ ImssInventory.API/Models/Computer.cs
@@ -4,30 +4,28 @@
     {
 
         public int Id { get; set; }
-        public string brand { get; set; }
-        public string  model { get; set; }
-        public string? serialNumber { get; set; }
-        public string? costsCenter { get; set; }
-        public int? adquisitionYear { get; set; }
-        public string? host { get; set; }
-        public string macAddress { get; set; }
-        public string ipAddress { get; set; }
-        public byte ipType { get; set; }
-        public string? processor { get; set; }
-        public string? speed { get; set; }
-        public string? hardDrive { get; set; }
-        public byte? ram { get; set; }
-        public string? operativeSystem { get; set; }
-        public byte? typeOs { get; set; }
-        public string? servicePack { get; set; }
-        public byte? activeLicence { get; set; }
-        public string? monitorBrand { get; set; }
-        public string? monitorModel { get; set; }
-        public string? monitorSerialNumber { get; set; }
-        public User user { get; set; }
-        public int userId { get; set; }
-
-
+        public string Brand { get; set; }
+        public string  Model { get; set; }
+        public string? SerialNumber { get; set; }
+        public string? CostsCenter { get; set; }
+        public int? AdquisitionYear { get; set; }
+        public string? Host { get; set; }
+        public string MacAddress { get; set; }
+        public string IpAddress { get; set; }
+        public byte IpType { get; set; }
+        public string? Processor { get; set; }
+        public string? Speed { get; set; }
+        public string? HardDrive { get; set; }
+        public byte? Ram { get; set; }
+        public string? OperativeSystem { get; set; }
+        public byte? TypeOs { get; set; }
+        public string? ServicePack { get; set; }
+        public byte? ActiveLicence { get; set; }
+        public string? MonitorBrand { get; set; }
+        public string? MonitorModel { get; set; }
+        public string? MonitorSerialNumber { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
 
 
 
---
+++ ImssInventory.API/Models/Position.cs
@@ -0,0 +1,9 @@
+﻿namespace ImssInventory.API.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
