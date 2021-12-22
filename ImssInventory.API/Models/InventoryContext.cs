c7f94da58c88ffb0f3a4a83f719f0b199ac2ef7b
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2021-12-21 22:27:04 -0800

    Was added InventoryContext to Models, Inject DBContext, Property definition.

---
+++ ImssInventory.API/Models/InventoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ImssInventory.API.Models
+{
+    public class InventoryContext:DbContext
+    {
+        public InventoryContext(DbContextOptions<InventoryContext> options):base(options)
+        {
+
+        }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Computer> Computer { get; set; }
+
+    }
+}
