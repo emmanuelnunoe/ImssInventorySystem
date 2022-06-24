710aa59bdff73ce5243e940e278cb8efd5df0ff4
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2022-06-23 18:25:12 -0700

    fix for recursion
    too much recursion fix

--- IMSSInventory.ClientApp/ClientApp/src/app/users/position/position.component.ts
+++ IMSSInventory.ClientApp/ClientApp/src/app/users/position/position.component.ts
@@ -1,57 +1,37 @@
-import { Position } from '@angular/compiler';
-import { Component, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import { FormControl, NgForm, ValidationErrors, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { pipe } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { PositionModel } from '../../core/models/position.model';
-import { PositionProvider } from "../../core/providers/position.provider";
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-position',
   templateUrl: './position.component.html',
   styleUrls: ['./position.component.css']
 })
-export default class PositionComponent implements OnInit, OnChanges {
+export default class PositionComponent  {
 
-  public positions: PositionModel[] = [];
-  public errorMessage: string = "";
-  public displayedColumns: string[] = ['id', 'positionName'];
-  public position: PositionModel = new PositionModel(0,"");
-  constructor(private positionProvider: PositionProvider, private _router: Router) {
-
-  }
-    ngOnChanges(changes: SimpleChanges): void {
-        throw new Error('Method not implemented.');
-    }
-
-  ngOnInit(): void {
-    this.getPositions();
-    this.position.id = undefined;
-    this.position.positionName = "";
+  constructor() {
 
   }
 
-  public deletePosition(positionId: number) {
-    this.positionProvider.deletePosition(positionId).subscribe({
-      complete: () => {
-        console.log("Se ha eliminado correctamente la posicion");
-        this.ngOnInit();
-      },
-      error: (error: any) => {
-        console.log(`Error al intentar eliminar la posicion ${error.message}`)
-      }
-    })
-  }
 
-  public getPositions() {
-    this.positionProvider.getPositions().subscribe((result: PositionModel[]) => {
-      this.positions = result;
-      console.log(this.positions);
-    }), (err: any) => {
-      console.log(err); 
-    } ;
-  }
+  //public deletePosition(positionId: number) {
+  //  this.positionProvider.deletePosition(positionId).subscribe({
+  //    complete: () => {
+  //      console.log("Se ha eliminado correctamente la posicion");
+  //      this.ngOnInit();
+  //    },
+  //    error: (error: any) => {
+  //      console.log(`Error al intentar eliminar la posicion ${error.message}`)
+  //    }
+  //  })
+  //}
+
+  //public getPositions() {
+  //  this.positionProvider.getPositions().subscribe((result: PositionModel[]) => {
+  //    this.positions = result;
+  //    console.log(this.positions);
+  //  }), (err: any) => {
+  //    console.log(err); 
+  //  } ;
+  //}
 
   //public savePosition(): void {
   //  console.log("creando nueva posicion..." + this.position);
